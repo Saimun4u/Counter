a62eb4696669b1011ae49c253718206d4d1e993e
Saimun <saimun4u@gmail.com>
2021-06-04 00:46:45 -0500

    select DOM elements and logic

---
+++ app.js
@@ -0,0 +1,6 @@
+//Select DOM Elements
+
+const board = document.getElementById("board");
+const increaseBtn = document.querySelector('.btn-increase');
+const increaseBtn = document.querySelector('.btn-decrease');
+const formInput = document.querySelector('.uservalue')

b175d90206c4eaebc500d2096d2c8617d3ffd445
Saimun <saimun4u@gmail.com>
2021-06-04 01:25:08 -0500

    Number change and logic set

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-//Select DOM Elements
+//Set initial count
+let count = 0;
 
-const board = document.getElementById("board");
-const increaseBtn = document.querySelector('.btn-increase');
-const increaseBtn = document.querySelector('.btn-decrease');
-const formInput = document.querySelector('.uservalue')
+//Select value and buttons
+
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach((btn)=>{
+    btn.addEventListener('click', e => {
+        // console.log(e.currentTarget.classList)
+        const style = e.currentTarget.classList; 
+        if (style.contains('increase')){
+            count++;
+        } else if (style.contains('decrease')){
+            count--;
+        }
+        else{
+            count = 0;
+        }
+        value.textContent =count;
+    })
+})
+

482140824cfdc1cd35d31037891aed9cb719b655
Saimun <saimun4u@gmail.com>
2021-06-04 01:41:35 -0500

    Finishing touches made

--- app.js
+++ app.js
@@ -18,6 +18,13 @@ btns.forEach((btn)=>{
         else{
             count = 0;
         }
+        if (count < 0){
+            value.style.color = 'red';
+        } else if (count > 0){
+            value.style.color = 'green'
+        }else{
+            value.style.color = 'black'
+        }
         value.textContent =count;
     })
 })
